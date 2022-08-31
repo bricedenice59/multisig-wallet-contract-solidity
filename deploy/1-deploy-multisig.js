23d3a39773e094c96dc789c162e01f624a01dde0
Brice Grenard <brice.grenard@gmail.com>
2022-08-31 17:02:12 +0800

    Do reorganize folder structure

--- deploy/1-deploy-multisig.js
+++ deploy/1-deploy-multisig.js
@@ -1,34 +1,33 @@
 require("dotenv").config();
 const { network } = require("hardhat");
-const { verify } = require("../utils/verify");
+const { verify } = require("../../marketplace/utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
-  const { deployer, owner1, owner2, owner3 } = await getNamedAccounts();
-  const chainId = network.config.chainId;
-  const listOfOwners = [owner1, owner2, owner3];
-  const args = listOfOwners;
+    const { deploy, log } = deployments;
+    const { deployer, owner1, owner2, owner3 } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    const listOfOwners = [owner1, owner2, owner3];
+    const args = listOfOwners;
 
-  log("---------Deploying contract....-------------");
-  console.log("owners passed in contract constructor are following:");
-  console.log(listOfOwners);
-  var deploymentResult = await deploy("MultiSig", {
-    from: deployer,
-    gasLimit: network.config.gasLimit,
-    args: [args],
-    log: true,
-    waitConfirmations:
-      network.config.blockConfirmationsForContractVerification || 1,
-  });
+    log("---------Deploying contract....-------------");
+    console.log("owners passed in contract constructor are following:");
+    console.log(listOfOwners);
+    var deploymentResult = await deploy("MultiSig", {
+        from: deployer,
+        gasLimit: network.config.gasLimit,
+        args: [args],
+        log: true,
+        waitConfirmations: network.config.blockConfirmationsForContractVerification || 1,
+    });
 
-  //only verifies on testnets
-  if (chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
-    log("Verifing contract on Etherscan!");
+    //only verifies on testnets
+    if (chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
+        log("Verifing contract on Etherscan!");
+        log("----------------------");
+        await verify(deploymentResult.address, [args]);
+        log("--------Verify Done--------------");
+    }
+
+    log("Contract deployed!");
     log("----------------------");
-    await verify(deploymentResult.address, [args]);
-    log("--------Verify Done--------------");
-  }
-
-  log("Contract deployed!");
-  log("----------------------");
 };
---
+++ deploy/99-update-frontend-contracts-contants.js
@@ -0,0 +1,21 @@
+const { ethers, network } = require("hardhat");
+const {
+    updateContractAddresses,
+    updateContractAbis,
+} = require("../../marketplace/utils/updateContractsAbisAndAdresses");
+
+const chainId = network.config.chainId.toString();
+module.exports = async function () {
+    if (process.env.UPDATE_FRONT_END) {
+        const contractName = "MultiSig";
+        const contract = await ethers.getContract(contractName);
+        console.log("updating front end contracts constants");
+
+        var contractAddress = contract.address;
+        var abi = contract.interface.format(ethers.utils.FormatTypes.json);
+        updateContractAddresses(contractName, contractAddress, chainId);
+        updateContractAbis(contractName, abi, chainId);
+    }
+};
+
+module.exports.tags = ["all", "updateFrontend"];
